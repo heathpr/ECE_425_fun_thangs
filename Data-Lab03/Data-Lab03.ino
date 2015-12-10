cc18c7b32c69c67f7221da6c8ac7e2af546c186b
Matthew <schackma@rose-hulman.edu>
2015-12-10 13:45:03 -0500

    thangs

--- Data-Lab03/Data-Lab03.ino
+++ Data-Lab03/Data-Lab03.ino
@@ -14,16 +14,17 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  long value;
-  pinMode(ftSonarPin, OUTPUT);//set the PING pin as an output
-  Robot.digitalWrite(ftSonarPin, LOW);//set the PING pin low first
-  delayMicroseconds(2);//wait 2 us
-  Robot.digitalWrite(ftSonarPin, HIGH);//trigger sonar by a 2 us HIGH PULSE
-  delayMicroseconds(5);//wait 5 us
-  Robot.digitalWrite(ftSonarPin, LOW);//set pin low first again
-  pinMode(ftSonarPin, INPUT);//set pin as input with duration as reception time
-  value = pulseIn(ftSonarPin, HIGH);//measures how long the pin is high
-  Robot.debugPrint(value);
-  delay(1000);
+//  long value;
+//  pinMode(ftSonarPin, OUTPUT);//set the PING pin as an output
+//  Robot.digitalWrite(ftSonarPin, LOW);//set the PING pin low first
+//  delayMicroseconds(2);//wait 2 us
+//  Robot.digitalWrite(ftSonarPin, HIGH);//trigger sonar by a 2 us HIGH PULSE
+//  delayMicroseconds(5);//wait 5 us
+//  Robot.digitalWrite(ftSonarPin, LOW);//set pin low first again
+//  pinMode(ftSonarPin, INPUT);//set pin as input with duration as reception time
+//  value = pulseIn(ftSonarPin, HIGH);//measures how long the pin is high
+//  value=Robot.analogRead(D8);
+//  Robot.debugPrint(value);
+//  delay(1000);
 
 }
