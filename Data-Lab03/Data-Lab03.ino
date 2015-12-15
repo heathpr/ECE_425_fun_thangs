d0995261d8ed40eb830d0d5fa861ff463e76630b
Matthew <schackma@rose-hulman.edu>
2015-12-15 15:01:11 -0500

    clean up

--- Data-Lab03/Data-Lab03.ino
+++ Data-Lab03/Data-Lab03.ino
@@ -1,41 +1,64 @@
+/* Lab03 Matthew Schack, Peter Heath, and Data
+    last edited 12/15/15
+
+    Impliments driving behaviors based on sensor input
+*/
+
+
 #include <ArduinoRobot.h>
 #include <math.h>
 
+//pins the sensors ar attached to
 #define SONAR_FRONT D1
 #define IR_FRONT_RIGHT M1
 #define IR_FRONT_LEFT M3
 #define SONAR_LEFT D2
 #define SONAR_RIGHT D0
 
+// min distances where good data can be read
 #define MIN_SONAR 483
 #define MAX_IR 595
 
+//number of samples in a burst
 #define NUM_SAMPLES 10
 
+//multipliers for shy kid behavior
 #define ANGLE_CALIBRATION 10
+#define MOVEMENT_CALIBRATION .3
+
 #define TURN_CALIBRATION 4.7 // time in ms to turn about 1 degree
-#define MOVEMENT_CALIBRATION .3  //mutiliplier for run away run away movement
-#define WAIT_TIME 50
-#define MAX_MOTOR_SPEED 255
-#define MAX_FORWARD_TIME 5000
-#define MAX_ANGLE 180
+
+#define WAIT_TIME 50 // time inbetween each main loop when choosing mode
+
 #define TURN_SPEED 150
 #define MOTOR_SPEED 200
+
 #define MOVE_TIME 250
+
 #define TRIGGER_RANGE 5
+
+#define MAX_MOTOR_SPEED 255
 #define MIN_MOTOR_SPEED 125
+
+//constants for random behavior
+#define MAX_FORWARD_TIME 5000
+#define MAX_ANGLE 180
 #define INCREMENT_FORWARD 50
 #define NUMBER_ITERATIONS 2
 #define RANDOM_SPEED 150
 
+
+void selectMode();
+void randomWander(void);
+void randomKid(int* i);
+void runAway(int def);
+void agressiveKid(void);
 double checkSonarPin(int pin);
 double checkIRPin(int pin);
-void goToAngle(int destination);
-void runAway(int def);
 void movement(double mag, double y);
-void randomWander(void);
-void selectMode(void);
-void agressiveKid(void);
+void goToAngle(int destination);
+
+
 
 
 void setup() {
@@ -54,54 +77,171 @@ void loop() {
 
 }
 
+void selectMode() {
+  Robot.text("Up for shy", 5, 5);
+  Robot.text("Left for aggressive", 5, 15);
+  Robot.text("Down for random wander", 5, 25);
+  Robot.text("Right for random obstacle", 5, 35);
+  int button = Robot.keyboardRead();
+  int i = 0;
+  switch (button) {
+    case BUTTON_UP: //shy kid
+      while (1) {
+        runAway(0);
+        Robot.clearScreen();
+        button = Robot.keyboardRead();
 
-void runAway(int def) {
-  double front_distance = checkSonarPin(SONAR_FRONT);
-  if (front_distance > TRIGGER_RANGE) {
-    front_distance = 0;
-  }
-  //  Robot.text("Front", 5, 50);
-  //  Robot.debugPrint(front_distance, 50, 50);
-  double front_left_distance = checkIRPin(IR_FRONT_LEFT);
-  if (front_left_distance > TRIGGER_RANGE) {
-    front_left_distance = 0;
-  }
-  //  Robot.text("Front left", 5, 59);
-  //  Robot.debugPrint(front_left_distance, 100, 59);
-  double front_right_distance = checkIRPin(IR_FRONT_RIGHT);
-  if (front_right_distance > TRIGGER_RANGE) {
-    front_right_distance = 0;
-  }
-  //  Robot.text("Front right", 5, 68);
-  //  Robot.debugPrint(front_right_distance, 100, 68);
-  double left_distance = checkSonarPin(SONAR_LEFT);
-  if (left_distance > TRIGGER_RANGE) {
-    left_distance = 0;
-  }
-  //  Robot.text("left", 5, 77);
-  //  Robot.debugPrint(left_distance, 50, 77);
-  double right_distance = checkSonarPin(SONAR_RIGHT);
-  if (right_distance > TRIGGER_RANGE) {
-    right_distance = 0;
-  }
-  //  Robot.text("right", 5, 86);
-  //  Robot.debugPrint(right_distance, 50, 86);
+        if (button == BUTTON_MIDDLE) { // break out of mode
+          Robot.motorsStop();
+          Robot.clearScreen();
+          break;
+        }
+      }
+      break;
+    case BUTTON_LEFT: //aggressive kid
+      Robot.clearScreen();
+      while (1) {
+        agressiveKid();
+        button = Robot.keyboardRead();
 
-  double y_vector = -(right_distance - left_distance + front_right_distance * sqrt(2) / 2 - front_left_distance * sqrt(2) / 2);
-  double x_vector = -(front_distance + front_right_distance * sqrt(2) / 2 + front_left_distance * sqrt(2) / 2);
-  //  Robot.debugPrint(x_vector, 5, 9);
-  //  Robot.debugPrint(y_vector, 5, 17);
+        if (button == BUTTON_MIDDLE) { //break out of mode
+          Robot.motorsStop();
+          Robot.clearScreen();
+          break;
+        }
+      }
+      break;
+    case BUTTON_DOWN: //random wander
+      i = 0;
+      while (1) {
+        Robot.clearScreen();
 
-  double mag = sqrt(x_vector * x_vector + y_vector * y_vector);
-  Robot.text("running away", 5, 1);
-  if ((mag < TRIGGER_RANGE && mag != 0)||def) {
-    movement(mag, y_vector);
-  } else {
-    Robot.debugPrint(mag, 5, 26);
+        randomWander(i);
+        button = Robot.keyboardRead();
+        i = i + 1;
+
+        if (button == BUTTON_MIDDLE) { //break out of mode
+          Robot.motorsStop();
+          Robot.clearScreen();
+          break;
+        }
+      }
+      break;
+    case BUTTON_RIGHT: //random wander with obsicle avoidance
+      i = 0;
+      while (1) {
+        Robot.clearScreen();
+
+        randomKid(&i);
+        button = Robot.keyboardRead();
+        i = i + 1;
+
+        if (button == BUTTON_MIDDLE) { //break out of mode
+          Robot.motorsStop();
+          Robot.clearScreen();
+          break;
+        }
+      }
+      break;
   }
 
 }
 
+// random wander behavior
+// angleIterations how many times the randomWander function has been run (kept track in the main loop)
+void randomWander(int angleIterations) {
+  if (angleIterations % NUMBER_ITERATIONS == 0) { // if we have iterated a predefined number of times then turn
+    int angle = random(-MAX_ANGLE, MAX_ANGLE);
+    goToAngle(angle);
+  }
+
+  // drive forward
+  Robot.motorsWrite(RANDOM_SPEED, RANDOM_SPEED); // random speed is just a slower speed NOT a random number for speed
+  delay(INCREMENT_FORWARD);
+
+}
+
+//random wander with obstacle avoidance behavior
+//i is the amount of times the randomWander function has been run
+void randomKid(int* i) {
+  Robot.text("running away", 5, 1);
+  //poll sensors
+  double front_distance = checkSonarPin(SONAR_FRONT);
+  double front_left_distance = checkIRPin(IR_FRONT_LEFT);
+  double front_right_distance = checkIRPin(IR_FRONT_RIGHT);
+
+  if (front_distance > TRIGGER_RANGE  && front_distance > TRIGGER_RANGE  && front_right_distance > TRIGGER_RANGE) { // if all of the sensors are outside the trigger range then random wander
+    Robot.debugPrint(*i, 5, 1);
+    randomWander(*i);
+  } else { // otherwise change i to force an angle change next iteration and run shy kid behavior
+    *i = NUMBER_ITERATIONS - 1; // -1 because after this look 1 will be added
+    Robot.motorsStop();
+    Robot.beep(BEEP_SIMPLE); // beep to let us know that we are avoiding an obsicle
+    runAway(1);
+  }
+
+}
+
+
+//shy kid behavior
+//def is whether the code is being run alone or in conjunction with random wander
+void runAway(int def) {
+  Robot.text("running away", 5, 1);
+
+  //poll all sensors and if they are greater than the TRIGGER_RAGE just set them to 0 (ignore the sensor)
+  double front_distance = checkSonarPin(SONAR_FRONT);
+  double right_distance = checkSonarPin(SONAR_RIGHT);
+  double front_left_distance = checkIRPin(IR_FRONT_LEFT);
+  double front_right_distance = checkIRPin(IR_FRONT_RIGHT);
+  double left_distance = checkSonarPin(SONAR_LEFT);
+
+  if (front_distance > TRIGGER_RANGE) {
+    front_distance = 0;
+  }
+  if (front_left_distance > TRIGGER_RANGE) {
+    front_left_distance = 0;
+  }
+  if (front_right_distance > TRIGGER_RANGE) {
+    front_right_distance = 0;
+  }
+  if (left_distance > TRIGGER_RANGE) {
+    left_distance = 0;
+  }
+  if (right_distance > TRIGGER_RANGE) {
+    right_distance = 0;
+  }
+
+  // vectors pushing away per where the obstacles are
+  double y_vector = -(right_distance - left_distance + front_right_distance * sqrt(2) / 2 - front_left_distance * sqrt(2) / 2);
+  double x_vector = -(front_distance + front_right_distance * sqrt(2) / 2 + front_left_distance * sqrt(2) / 2);
+
+  //magnitude of vectors added together
+  double mag = sqrt(x_vector * x_vector + y_vector * y_vector);
+
+  if ((mag < TRIGGER_RANGE && mag != 0) || def) { // if magnitude is less than the trigger range and not 0 (all sensored ignored) OR we are running this with another function
+    movement(mag, y_vector); // move the motors
+  } else {
+    Robot.debugPrint(mag, 5, 26); // print the magnitude if it is too low to do anything
+  }
+}
+
+//aggressive kid behavior
+void agressiveKid() {
+  Robot.text("agressive kid", 5, 5);
+
+  bool isClear = false;
+  int range = checkSonarPin(SONAR_FRONT);
+  if (range <= TRIGGER_RANGE && range != 0) { // if within trigger range set value to stop
+    isClear = false;
+  } else {
+    isClear = true; // set value to go
+  }
+  Robot.motorsWrite(isClear * MOTOR_SPEED, isClear * MOTOR_SPEED); // will be 0 if false
+  delay(WAIT_TIME);
+  Robot.clearScreen();
+}
+
+// polls the given pin 10 times and averages the result
 double checkSonarPin(int pin) {
   double output = 0;
   for (int i = 0; i < NUM_SAMPLES; i++) {
@@ -126,6 +266,7 @@ double checkSonarPin(int pin) {
 }
 
 
+// polls the given pin 10 times and averages the result
 double checkIRPin(int pin) {
   double output = 0;
   for (int i = 0; i < NUM_SAMPLES; i++) {
@@ -142,39 +283,27 @@ double checkIRPin(int pin) {
   return output;
 }
 
-
+// moves according to the potential fields method with the given magnitude and y vector
+//only moves backwards
 void movement(double mag, double y) {
-  float move_spd = (5 - mag) * MOTOR_SPEED * MOVEMENT_CALIBRATION;
+  
+  float move_spd = (TRIGGER_RANGE - mag) * MOTOR_SPEED * MOVEMENT_CALIBRATION; // P control for the base speed
+
+  // keep the movement speed within bounds and high enough to move
   if (move_spd < MIN_MOTOR_SPEED) {
     move_spd = MIN_MOTOR_SPEED;
   }
-  if(move_spd > MAX_MOTOR_SPEED){
+  if (move_spd > MAX_MOTOR_SPEED) {
     move_spd = MOTOR_SPEED;
   }
-  Robot.motorsWrite(-move_spd + y * ANGLE_CALIBRATION, -move_spd - y * ANGLE_CALIBRATION);
-  Robot.debugPrint(-move_spd + y * ANGLE_CALIBRATION, 5, 26);
-  Robot.debugPrint(-move_spd - y * ANGLE_CALIBRATION, 5, 35);
+  Robot.motorsWrite(-move_spd + y * ANGLE_CALIBRATION, -move_spd - y * ANGLE_CALIBRATION); // differential for turning
   delay(MOVE_TIME);
   Robot.motorsStop();
 }
 
-void randomWander(int angleIterations) {
-  int angle = 0;
-  if (angleIterations % NUMBER_ITERATIONS == 0) {
-    angle = random(-MAX_ANGLE, MAX_ANGLE);
-    //change angle
-    //    Robot.motorsStop();
-    //    Robot.turn(angle);
-    goToAngle(angle);
-  }
-  // drive forward
-  Robot.motorsWrite(RANDOM_SPEED, RANDOM_SPEED);
-  delay(INCREMENT_FORWARD);
 
-}
-
+//goes to the given angle
 void goToAngle(int angle) {
-  Robot.debugPrint(angle, 5, 100);
   Robot.motorsStop();
   float delayTime = angle * TURN_CALIBRATION; //find time to turn
   // decides what direction to turn
@@ -187,108 +316,3 @@ void goToAngle(int angle) {
   }
   Robot.motorsStop();
 }
-
-void selectMode() {
-  Robot.text("Up for shy", 5, 5);
-  Robot.text("Left for aggressive", 5, 15);
-  Robot.text("Down for random wander", 5, 25);
-  Robot.text("Right for random obstacle", 5, 35);
-  int button = Robot.keyboardRead();
-  int i = 0;
-  switch (button) {
-    case BUTTON_UP:
-      while (1) {
-        runAway(0);
-        Robot.clearScreen();
-        button = Robot.keyboardRead();
-        if (button == BUTTON_MIDDLE) {
-          Robot.motorsStop();
-          Robot.clearScreen();
-          break;
-        }
-      }
-      break;
-    case BUTTON_LEFT:
-      Robot.clearScreen();
-      while (1) {
-        agressiveKid();
-        int button = Robot.keyboardRead();
-        if (button == BUTTON_MIDDLE) {
-          Robot.motorsStop();
-          Robot.clearScreen();
-          break;
-        }
-      }
-      break;
-    case BUTTON_DOWN:
-      i = 0;
-      while (1) {
-        Robot.clearScreen();
-        randomWander(i);
-        button = Robot.keyboardRead();
-        if (button == BUTTON_MIDDLE) {
-          Robot.motorsStop();
-          Robot.clearScreen();
-          break;
-        }
-        i = i + 1;
-      }
-      break;
-    case BUTTON_RIGHT:
-      i = 0;
-      while (1) {
-        Robot.clearScreen();
-
-        randomKid(&i);
-        button = Robot.keyboardRead();
-        if (button == BUTTON_MIDDLE) {
-          Robot.motorsStop();
-          Robot.clearScreen();
-          break;
-        }
-        i = i + 1;
-      }
-      break;
-  }
-
-}
-
-void agressiveKid() {
-  Robot.text("agressive kid", 5, 5);
-  bool isClear = false;
-  int range = checkSonarPin(SONAR_FRONT);
-  if (range <= TRIGGER_RANGE) {
-    if (range == 0) {
-      isClear = true;
-    } else {
-      isClear = false;
-    }
-  } else {
-    isClear = true;
-  }
-
-  Robot.motorsWrite(isClear * MOTOR_SPEED, isClear * MOTOR_SPEED);
-  range = checkSonarPin(SONAR_FRONT);
-  delay(WAIT_TIME);
-  Robot.clearScreen();
-}
-
-void randomKid(int* i) {
-  double front_distance = checkSonarPin(SONAR_FRONT);
-  double front_left_distance = checkIRPin(IR_FRONT_LEFT);
-  double front_right_distance = checkIRPin(IR_FRONT_RIGHT);
-  Robot.debugPrint(front_distance, 5, 120);
-  Robot.debugPrint(front_left_distance, 5, 129);
-  Robot.debugPrint(front_right_distance, 5, 129);
-  if (front_distance > TRIGGER_RANGE  && front_distance > TRIGGER_RANGE  && front_right_distance > TRIGGER_RANGE) {
-    Robot.debugPrint(*i,5,1);
-    randomWander(*i);
-  } else {
-    *i = NUMBER_ITERATIONS - 1;
-    Robot.motorsStop();
-    Robot.beep(BEEP_SIMPLE);
-    runAway(1);
-  }
-
-}
-
