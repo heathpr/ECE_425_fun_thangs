9b078758e61c7d918e268a1cca7f77a310092937
peter heath <heathpr@rose-hulman.edu>
2015-12-15 12:50:56 -0500

    updated calibration and shell of lab 4

---
+++ lab-04/sonar_test/sonar_test.ino
@@ -0,0 +1,68 @@
+#include <ArduinoRobot.h>
+
+#define SONAR_FRONT D1
+#define IR_FRONT_RIGHT M1
+#define IR_FRONT_LEFT M3
+#define SONAR_LEFT D2
+#define SONAR_RIGHT D0
+#define MIN_SONAR 483
+#define MAX_IR 595
+#define NUM_SAMPLES 5
+
+void setup() {
+  // put your setup code here, to run once:
+  Robot.begin();
+  Robot.beginTFT();
+  Serial.begin(9600);
+  // print some text to the screen
+  Robot.stroke(0, 0, 0);
+  Robot.beginSpeaker();
+
+}
+
+double checkIRPin(int pin) {
+  double output = 0;
+  for (int i = 0; i < NUM_SAMPLES; i++) {
+    int value;
+    value = Robot.analogRead(pin);
+    Robot.debugPrint(value, 5, 40);
+    value = 2517.3 / value - 1.7528;
+    output += value;
+    delay(5);
+  }
+  output = output / NUM_SAMPLES;
+  if (output < 0) {
+    output = 0;
+  }
+  return output;
+}
+
+double checkSonarPin(int pin) {
+  double output = 0;
+  for (int i = 0; i < NUM_SAMPLES; i++) {
+    int value;
+    pinMode(pin, OUTPUT);//set the PING pin as an output
+    Robot.digitalWrite(pin, LOW);//set the PING pin low first
+    delayMicroseconds(2);//wait 2 us
+    Robot.digitalWrite(pin, HIGH);//trigger sonar by a 2 us HIGH PULSE
+    delayMicroseconds(5);//wait 5 us
+    Robot.digitalWrite(pin, LOW);//set pin low first again
+    pinMode(pin, INPUT);//set pin as input with duration as reception time
+    value = pulseIn(pin, HIGH);//measures how long the pin is high
+    Robot.debugPrint(value, 5, 40);
+    value = .0111 * value - 0.8107;
+    output += value;
+    delay(5);
+  }
+  output = output / NUM_SAMPLES;
+  if (output < 0) {
+    output = 0;
+  }
+  return output;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  
+}
