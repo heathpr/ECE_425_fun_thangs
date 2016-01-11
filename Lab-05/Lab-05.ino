7ebfe73c05c8736469bfa77f371ca68d55886ce1
peter heath <heathpr@rose-hulman.edu>
2016-01-11 13:26:36 -0500

    Lab05 more things work except not really

--- Lab-05/Lab-05.ino
+++ Lab-05/Lab-05.ino
@@ -3,10 +3,13 @@
 
 #define PHOTO_LEFT B_TK2
 #define PHOTO_RIGHT B_TK4
-
+#define MAX_MOTOR_SPEED 255
+#define MIN_MOTOR_SPEED 0
+#define KP 3000
 int right = -1;
 int left = -1;
 int readPhotoSensor(int pin);
+void attraction(void);
 
 void setup() {
   Robot.begin();
@@ -17,12 +20,8 @@ void setup() {
 }
 
 void loop() {
-  right=readPhotoSensor(PHOTO_RIGHT);
-   delay(1000);
-  Robot.clearScreen();
-  left=readPhotoSensor(PHOTO_LEFT);
-  
-  delay(1000);
+  attraction();
+  delay(15);
   Robot.clearScreen();
 
 }
@@ -30,7 +29,28 @@ void loop() {
 int readPhotoSensor(int pin) {
   int value;
   value = Robot.analogRead(pin);
-  Robot.debugPrint(value,5,30);
   return value;
+}
+
+void attraction() {
+  int rightDist = -1;
+  int leftDist = -1;
+  int leftSpeed;
+  int rightSpeed;
+  right = readPhotoSensor(PHOTO_RIGHT);
+  left = readPhotoSensor(PHOTO_LEFT);
+  rightDist = 154.948 - 0.167 * right;
+  leftDist = 108.215 - 0.120 * left;
+  rightSpeed = 1 / rightDist * KP + MIN_MOTOR_SPEED;
+  leftSpeed = 1 / leftDist * KP + MIN_MOTOR_SPEED;
+  if (rightSpeed > MAX_MOTOR_SPEED) {
+    rightSpeed = MAX_MOTOR_SPEED;
+  }
+  if (leftSpeed > MAX_MOTOR_SPEED) {
+    leftSpeed = MAX_MOTOR_SPEED;
+  }
+  Robot.debugPrint(rightSpeed, 5, 20);
+  Robot.debugPrint(leftSpeed, 5, 80);
+  Robot.motorsWrite(leftSpeed, rightSpeed);
 }
 
